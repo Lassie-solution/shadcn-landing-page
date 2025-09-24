fe8606c70ed6c96fd9671cc0ebded366c91f1ac8
vzvarich681 <vzvarich681@gmail.com>
2025-09-24 03:36:14 +0300

    refactor: uncomment previously commented imports for component usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-// import { About } from "./components/About";
-// import { Cta } from "./components/Cta";
-// import { FAQ } from "./components/FAQ";
-// import { Features } from "./components/Features";
-// import { Footer } from "./components/Footer";
+import { About } from "./components/About";
+import { Cta } from "./components/Cta";
+import { FAQ } from "./components/FAQ";
+import { Features } from "./components/Features";
+import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-// import { HowItWorks } from "./components/HowItWorks";
+import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-// import { Newsletter } from "./components/Newsletter";
-// import { Pricing } from "./components/Pricing";
+import { Newsletter } from "./components/Newsletter";
+import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
-// import { Services } from "./components/Services";
-// import { Sponsors } from "./components/Sponsors";
-// import { Team } from "./components/Team";
-// import { Testimonials } from "./components/Testimonials";
+import { Services } from "./components/Services";
+import { Sponsors } from "./components/Sponsors";
+import { Team } from "./components/Team";
+import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      {/* <Sponsors />
+      <Sponsors />
       <About />
       <HowItWorks />
       <Features />
@@ -31,7 +31,7 @@ function App() {
       <Pricing />
       <Newsletter />
       <FAQ />
-      <Footer /> */}
+      <Footer />
       <ScrollToTop />
     </>
   );
